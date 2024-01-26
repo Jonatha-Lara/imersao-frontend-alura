5b3c9d0fcf0f45c844cad41374fe766ffee2b548
DevJonatha <jonathalarasilverio@gmail.com>
2024-01-26 00:23:16 -0300

    aula 4

---
+++ script.js
@@ -0,0 +1,33 @@
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylists = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
+
+function requestApi(searchTerm) {
+  const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+  fetch(url)
+    .then((response) => response.json())
+    //.then((results) => displayResults(results));
+}
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultArtist.classList.add('hidden');
+        resultPlaylists.classList.remove('hidden');
+        return;
+    }
+    requestApi(searchTerm);
+});
+
+
+//function displayResults(results) {
+//  hidePlaylists();
+//  const artistImage = document.getElementById("artist-img");
+//  const artistName = document.getElementById("artist-name");
+
+//  results.forEach((element) => {
+//    artistImage.src = element.urlImg;
+//    artistName.innerText = element.name;
+//  });
+//  resultArtist.classList.remove("hidden");
+//}
